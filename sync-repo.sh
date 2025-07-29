59ed3941127bd2782cbaf3d6169b432ddd70403c
Andrew C. Scoppa <andrew.scoppa@gmail.com>
2025-07-29 08:52:50 -0400

    Initial commit

---
+++ sync-repo.sh
@@ -0,0 +1,17 @@
+# This bash script uses git to synchronize changes between the local and remote GitHub repository.
+# Usage: ./sync-repo.sh
+
+# steps:
+# 1. stage all changes
+# 2. commit changes with message 'Updated'
+# 3. pull changes from remote repository on branch 'main'
+# 4. push changes to remote repository on branch 'main'.
+# 5. check if the push was successful
+
+
+
+
+
+
+
+
